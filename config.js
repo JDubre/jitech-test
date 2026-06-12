268e414dd131fa28dff60e683520ed6047c6a60e
JDubre <jorge.dubreuil@gmail.com>
2026-06-12 16:11:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@
 window.JITECH_CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbys98aYPXHAjJ7vSB7ttjmiVwrzGKpVnl45W1pe93R5XH3z6coRUG1IZQP9t92m-zee/exec",
   TEST_DURATION_MINUTES: 20,
-  APP_VERSION: "JITECH-PWA-AUTO-2026-06-12",
+  APP_VERSION: "JITECH-PWA-AUTO-2026-06-12-FIX-PAYLOAD",
   RESULT_MODE: "auto-email-sheet"
 };
+
